b6a958d00132c428a48396a48dd7c90c18fc70bd
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-05-06 20:17:25 -0700

    add basic security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < ApplicationController
+
+  # adding basic http authentication for admins
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
+  password: ENV['HTTP_BASIC_PASSWORD'],
+  if: -> { ENV['HTTP_BASIC_PASSWORD'].present? }
+
   def show
+  # controller fetches data, makes it available to the views automagically
+  @product_count = Product.count
+  @category_count = Category.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
+  # adding basic http authentication for admins
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
+  password: ENV['HTTP_BASIC_PASSWORD'],
+  if: -> { ENV['HTTP_BASIC_PASSWORD'].present? }
+
   def index
     @products = Product.order(id: :desc).all
   end
